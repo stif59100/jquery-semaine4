5f24ac86db13d0d6e016de5b1b7522cbb3275f8e
steeve <steeve.vanderstocken@gmail.com>
2021-10-25 09:43:15 +0200

    jour 4 j5 semaine 3

---
+++ Javascript-dom/jour-5/form-tools.js
@@ -0,0 +1,18 @@
+function formToObject (formElement) {
+    // Récupération de la saisie
+    postedData = new FormData(formElement);
+
+    // On peut accèder au valeurs saisies avec ma méthode get
+    // console.log(postedData.get("age"));
+    // console.log(postedData.get("firstName"));
+
+    const data = {};
+
+    for (info of postedData.entries()) {
+        const key = info[0];
+        const val = info[1];
+        data[key] = val;
+    }
+
+    return data;
+}// fin de la fonction formToObject
